00884cd34064890f645f1243e38d8bc758899677
Blaise Wang <blaisewang@icloud.com>
2017-04-16 16:56:02 +0800

    remove unused layers

--- Admin.aspx.cs
+++ Admin.aspx.cs
@@ -1,16 +1,7 @@
 ﻿using System;
-using System.Collections;
-using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
-using System.Web.Security;
-using System.Web.UI;
-using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
-using System.Web.UI.WebControls.WebParts;
-using System.Xml.Linq;
 
 public partial class Admin : System.Web.UI.Page
 {
--- Cart.aspx.cs
+++ Cart.aspx.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 using System.Collections;
 using System.Data;
 using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
 public partial class Cart : System.Web.UI.Page
 {
